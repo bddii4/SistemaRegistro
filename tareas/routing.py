a8ced62051c2193e6e961bd5e144c44ae497511b
bddii4 <karen.abigail1528@gmail.com>
2026-05-14 12:28:14 -0600

    Rutas WebSocket

--- tareas/routing.py
+++ tareas/routing.py
@@ -1,4 +1,4 @@
-# Rutas WebSocket: /ws/monitor/ para conexión en tiempo real.
+# Rutas WebSocket: /ws/monitor/ para conexión en tiempo real
 from django.urls import re_path
 from tareas import consumers
 
