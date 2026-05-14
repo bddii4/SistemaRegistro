519db2ede1579bc0856a186f6c7e6819cd483eaa
bddii4 <karen.abigail1528@gmail.com>
2026-05-14 12:22:37 -0600

    URLs raíz (admin, login, logout)

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-# URLs raíz: admin de Django, login/logout, e inclusion de rutas de tareas.
+# URLs raíz: admin de Django, login/logout, e inclusion de rutas de tareas
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
