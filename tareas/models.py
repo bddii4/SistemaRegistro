a3e2bd3025e57cd01a3deca5ea12b1a73cec5d9a
bddii4 <karen.abigail1528@gmail.com>
2026-05-14 12:24:46 -0600

    Modelos: Usuario, Reporte, SolicitudReporte

--- tareas/models.py
+++ tareas/models.py
@@ -1,4 +1,4 @@
-# Modelos: Usuario (login/roles), Reporte (actividad del empleado), SolicitudReporte (solicitudes manuales/auto).
+# Modelos: Usuario (login/roles), Reporte (actividad del empleado), SolicitudReporte (solicitudes manuales/auto)
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
